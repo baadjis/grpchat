6b3ffaa6ceed034daa6f12fcf03aab2526a9d7ae
baadjis <baadjisidy@gmail.com>
2020-09-24 15:17:45 +0200

    new functionalities in client.go and server.go

--- client.go
+++ client.go
@@ -1,34 +1,278 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
 	"github.com/baadjis/grpchat/chat"
+	"github.com/fatih/color"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
+type Listener struct {
+	MessageChanel    chan chat.Message
+	MessageWaitGroup *sync.WaitGroup
+	chattingState    bool
+	stream           chat.ChatService_RouteChatClient
+	SignalWaitGroup  *sync.WaitGroup
+	SignalChanel     chan os.Signal
+}
+
+func NewListener() *Listener {
+	listener := &Listener{
+		chattingState:    false,
+		MessageChanel:    make(chan chat.Message),
+		stream:           nil,
+		MessageWaitGroup: &sync.WaitGroup{},
+		SignalChanel:     make(chan os.Signal),
+		SignalWaitGroup:  &sync.WaitGroup{},
+	}
+	listener.MessageWaitGroup.Add(1)
+	listener.SignalWaitGroup.Add(1)
+	return listener
+}
+
+func (s *Listener) StopListeningMessage() {
+	log.Print("[Stop]: Entered Stop.")
+	log.Print(s.SignalChanel)
+	close(s.SignalChanel)
+	log.Print("[Stop]: Waiting.")
+	s.MessageWaitGroup.Wait()
+	log.Print("[Stop]: Done.")
+}
+
+// ControlExit handles any interrupts during program execution.
+// Note: The routine control is dictated by the existence of a stream. If one is present, the user is in a group and needs
+// to be removed. Otherwise, the user is still in the menu system.
+// It doesn't return anything.
+func (s *Listener) ControlExit(c chat.ChatServiceClient, u string, g string) {
+
+	log.Print("[ControlExit]: Entered.")
+
+	defer s.SignalWaitGroup.Done()
+	signal.Notify(s.SignalChanel, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-s.SignalChanel:
+
+			if s.chattingState {
+				log.Print("I am still chatting.")
+				s.stream.Send(&chat.Message{Sender: u, Receiver: g, Body: u + " left chat!\n"})
+				ExitClient(c, u, g)
+				return
+			}
+
+			log.Print("I am no longer chatting.")
+			ExitClient(c, u, g)
+			os.Exit(1)
+			return
+		}
+	}
+}
+
+// ExitClient handles removing the client from the server and exiting the program.
+// It doesn't return anything.
+func ExitClient(c chat.ChatServiceClient, u string, g string) {
+
+	c.UnRegister(context.Background(), &chat.ChatClient{Sender: u})
+	os.Exit(1)
+}
+
+// ListenToClient listens to the client for input and adds that input to the sQueue with
+// the username of the sender, group name, and the message.
+// It doesn't return anything.
+func ListenToClient(messagesQueue *Listener, reader *bufio.Reader, uName string, gName string) {
+
+	log.Println("Start listening.")
+	defer messagesQueue.MessageWaitGroup.Done()
+
+	for {
+		msg, _ := reader.ReadString('\n')
+		if strings.TrimSpace(msg) == "!leave" {
+			log.Println("Stop listening chat.")
+			messagesQueue.MessageChanel <- chat.Message{Sender: uName, Body: msg, Receiver: gName}
+			return
+		}
+		log.Println("Adding message to the queue.")
+		messagesQueue.MessageChanel <- chat.Message{Sender: uName, Body: msg, Receiver: gName}
+	}
+}
+
+// ReceiveMessages listens on the client's (NOT the client's group) stream and adds any incoming
+// message to the client's inbox.
+// It doesn't return anything.
+func ReceiveMessages(inbox *Listener, stream chat.ChatService_RouteChatClient, u string) {
+
+	log.Println("[ReceiveMessages]: Starting.")
+	defer inbox.MessageWaitGroup.Done()
+
+	for {
+		log.Println("listening for new messages.")
+		msg, _ := stream.Recv()
+		log.Println("Received message: " + msg.Body)
+		if msg.Body == u+" left chat!\n" {
+			log.Println("Found special signal!")
+			return
+		}
+
+		inbox.MessageChanel <- *msg
+	}
+}
+
+// DisplayCurrentMembers displays the members who are currently in the group chat.
+// It doesn't return anything.
+func CurrentMembers(c chat.ChatServiceClient, g string) {
+
+	m, _ := c.GetChatGroupClientList(context.Background(), &chat.ChatGroup{Name: g})
+	if len(m.Clients) > 0 {
+		fmt.Print("Current Members: ")
+		for i := 0; i < len(m.Clients); i++ {
+			if i == len(m.Clients)-1 {
+				fmt.Print(m.Clients[i])
+			} else {
+				fmt.Print(m.Clients[i] + ", ")
+			}
+		}
+
+	}
+}
+
+//initialise message listener and start chatting
+
+func StartChat(stream chat.ChatService_RouteChatClient, c chat.ChatServiceClient, listener *Listener, r *bufio.Reader, u string, g string) *Listener {
+
+	//t, cancel := context.WithCancel(ctx)
+	//stream, serr := c.RouteChat(t)
+	//defer cancel()
+
+	CurrentMembers(c, g)
+
+	//if serr != nil {
+	//	fmt.Print(serr)
+	//} else {
+	listenerQueue := NewListener() // Creates the sQueue with a channel and waitgroup.
+
+	go ListenToClient(listenerQueue, r, u, g)
+	go ReceiveMessages(listenerQueue, stream, u)
+
+	// TODO: Find out why the first message is always dropped so an empty message needn't be sent.
+	stream.Send(&chat.Message{Sender: u, Receiver: g, Body: ""})
+	stream.Send(&chat.Message{Sender: u, Receiver: g, Body: "joined chat!\n"})
+	listener.chattingState = true
+	listener.stream = stream
+	return listenerQueue
+
+}
+
+func Chat(conn *grpc.ClientConn, stream chat.ChatService_RouteChatClient, c chat.ChatServiceClient, listener *Listener, r *bufio.Reader, u string, g string) bool {
+
+	listenerQueue := StartChat(stream, c, listener, r, u, g)
+	AddSpacing(1)
+	fmt.Println("good chat with " + g + ".")
+	Frame()
+
+	for {
+		select {
+		case toSend := <-listenerQueue.MessageChanel:
+			switch msg := strings.TrimSpace(toSend.Body); msg {
+			case "!members":
+				log.Println("!members.")
+				CurrentMembers(c, g)
+			case "!leave":
+				log.Println("!leave.")
+				c.LeaveChatRoom(context.Background(), &chat.ChatGroup{Client: u, Name: g})
+				listenerQueue.StopListeningMessage()
+
+				//stream.CloseSend()
+				log.Println("HEY LOOK")
+				//cancel()
+				log.Println(context.Canceled)
+				return true
+			case "!exit":
+				log.Println("!exit.")
+				stream.Send(&chat.Message{Sender: u, Receiver: g, Body: u + " left chat!\n"})
+				ExitClient(c, u, g)
+				//stream.CloseSend()
+				//cancel()
+				conn.Close()
+				return false
+			case "!help":
+				log.Println("[Main]: I'm in !help.")
+				AddSpacing(1)
+				fmt.Println("The following commands are available to you: ")
+				color.New(color.FgHiYellow).Print("   !members")
+				fmt.Print(": Lists the current members in the group.")
+
+				AddSpacing(1)
+				color.New(color.FgHiYellow).Print("   !exit")
+				fmt.Println(": Leaves the chat server.")
+				AddSpacing(1)
+
+			default:
+				log.Println("[Main]: Sending the message.")
+				stream.Send(&toSend)
+			}
+		case received := <-listenerQueue.MessageChanel:
+			log.Println("[Receiving the message.")
+			if received.Body != "!leave" {
+				fmt.Printf("%s> %s", received.Sender, received.Body)
+			}
+		}
+	}
+
+}
 func main() {
 
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	r := bufio.NewReader(os.Stdin)
+
+	var uName string // Client username
+	var gName string // Client's chat group
+
+	a := SetServer(r)
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(a, grpc.WithInsecure())
+
 	if err != nil {
-		log.Fatalf("did not connect: %s", err)
+		log.Fatalf("Could not connect: %v", err)
+	} else {
+		fmt.Printf("\nYou have successfully connected to %s! To disconnect, hit ctrl+c or type !exit.\n\n", a)
 	}
+
+	// Close the connection after main returns.
 	defer conn.Close()
 
+	// Create the client
 	c := chat.NewChatServiceClient(conn)
-
-	response, err := c.SayHello(context.Background(), &chat.Message{Body: "Hello From Client!"})
-	if err != nil {
-		log.Fatalf("Error when calling SayHello: %s", err)
+	ctx := context.Background()
+	stream, serr := c.RouteChat(ctx)
+	if serr != nil {
+		log.Fatal(serr)
 	}
-	log.Printf("Response from server: %s", response.Body)
 
-	response, err = c.BroadcastMessage(context.Background(), &chat.Message{Body: "Message to Broadcast!"})
-	if err != nil {
-		log.Fatalf("Error when calling Broadcast Message: %s", err)
+	uName = SetName(c, r)
+	showMenu := true // Control whether the user sees the menu or exits.
+	m := NewListener()
+	go m.ControlExit(c, uName, gName)
+
+	for showMenu {
+		gName, err = TopMenu(c, r, uName)
+
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+
+		log.Print("Showing context")
+		log.Print(ctx)
+		showMenu = Chat(conn, stream, c, m, r, uName, gName)
 	}
-	log.Printf("Response from server: %s", response.Body)
-
 }
--- server.go
+++ server.go
@@ -1,30 +1,517 @@
 package main
 
 import (
+	"crypto/rand"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"sync"
 
 	"github.com/baadjis/grpchat/chat"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
+
+// The port the server is listening on.
+const (
+	port        = ":12021"
+	tokenHeader = "x-chat-token"
+)
+
+// the server
+type server struct {
+	Host, Password string
+	lock           sync.RWMutex
+	chatclients    map[string]*Client
+	chatgroups     map[string]*Group
+	clienttoken    map[string]string
+}
+
+type Group struct {
+	name      string
+	ch        chan chat.Message
+	clients   []string
+	WaitGroup *sync.WaitGroup
+}
+
+type Client struct {
+	name      string
+	groups    []string
+	ch        chan chat.Message
+	WaitGroup *sync.WaitGroup
+}
+
+// AddClient adds a new client n to the server.
+
+func (s *server) AddChatClient(n string) {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	c := &Client{
+		name:      n,
+		ch:        make(chan chat.Message, 100),
+		WaitGroup: &sync.WaitGroup{},
+	}
+
+	log.Print("added client " + n)
+	s.chatclients[n] = c
+}
+
+//  add a new group to the server.
+
+func (s *server) AddChatGroup(n string) {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	g := &Group{
+		name:      n,
+		ch:        make(chan chat.Message, 100),
+		WaitGroup: &sync.WaitGroup{},
+	}
+
+	log.Print("Added a chat group " + g.name)
+	s.chatgroups[n] = g
+	s.chatgroups[n].WaitGroup.Add(1)
+}
+
+// checks if a client name already exists on the server.
+
+func (s *server) RegisteredClient(n string) bool {
+
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	for c := range s.chatclients {
+		if c == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+//checks if a group name already exists on the server.
+
+func (s *server) NotAvailableGroupName(groupName string) bool {
+
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	for group := range s.chatgroups {
+		if group == groupName {
+			return true
+		}
+	}
+
+	return false
+}
+
+//cheks if a given client joined a given group
+func (s *server) ClientJoinedGroup(clientName string, groupName string) bool {
+
+	for _, c := range s.chatgroups[groupName].clients {
+
+		if clientName == c {
+			return true
+		}
+	}
+	return false
+}
+
+// remove a client form a given group
+func (s *server) RemoveClientFromGroup(clientName string, groupName string) error {
+
+	if s.ClientJoinedGroup(clientName, groupName) {
+		// remove group name from client group
+		c := s.chatclients[clientName].groups
+		for i, g := range c {
+			if groupName == g {
+				c[i] = c[len(c)-1]
+				c = c[:len(c)-1]
+				s.chatclients[clientName].groups = c
+
+				// remove the group if there is one client
+				if len(s.chatgroups[g].clients) == 1 {
+					delete(s.chatgroups, g)
+				} else {
+
+					//remove  client name  from the group client
+
+					gc := s.chatgroups[g].clients
+					for j, cl := range gc {
+						if cl == clientName {
+
+							gc[j] = gc[len(gc)-1]
+							gc = gc[:len(gc)-1]
+							s.chatgroups[g].clients = gc
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+		return nil
+	}
+	return errors.New("can not found use with this name in this group ")
+}
+
+// remove client from any chat group
+
+func (s *server) RemoveClient(clientName string) error {
+
+	// TODO: There is some deadlock here when a user attempts to quit
+	// 		 the chat app with !exit.
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if s.RegisteredClient(clientName) {
+
+		delete(s.chatclients, clientName)
+
+		for _, g := range s.chatclients[clientName].groups {
+
+			s.RemoveClientFromGroup(clientName, g)
+		}
+
+	}
+	return errors.New("Client (" + clientName + ") is not registred")
+}
+
+// add a client to a group.
+
+func (s *server) AddClientToChatGroup(clientName string, groupName string) error {
+
+	//lock.Lock()
+	//defer lock.Unlock()
+
+	s.chatgroups[groupName].WaitGroup.Add(1)
+	defer s.chatgroups[groupName].WaitGroup.Done()
+	// group did not exist
+	if !s.NotAvailableGroupName(groupName) {
+		return errors.New("Group (" + groupName + ") did not exist")
+	}
+
+	// client is not registered yet
+
+	if !s.RegisteredClient(clientName) {
+		return errors.New("Client(" + clientName + ") is not registered")
+	}
+	// client already joined group
+	if s.ClientJoinedGroup(clientName, groupName) {
+		return errors.New("Client(" + clientName + ") already joined group(" + groupName + ")")
+	}
+	s.chatgroups[groupName].clients = append(s.chatgroups[groupName].clients, clientName)
+	s.chatclients[clientName].groups = append(s.chatclients[clientName].groups, groupName)
+
+	log.Println("Added " + clientName + " to group" + groupName)
+	return nil
+}
+
+// get all of the currently connected clients to the server.
+func (s *server) GetChatClientList(ctx context.Context, in *chat.Empty) (*chat.ChatClientList, error) {
+
+	var cl []string
+	for key := range s.chatclients {
+		cl = append(cl, key)
+	}
+
+	log.Print("this is the list of current clients ")
+	log.Print(cl)
+
+	return &chat.ChatClientList{Clients: cl}, nil
+}
+
+// It returns a list of  all chatgroups.
+func (s *server) GetChatGroupList(ctx context.Context, in *chat.Empty) (*chat.ChatGroupList, error) {
+
+	var grp []string
+	for groupName := range s.chatgroups {
+		grp = append(grp, groupName)
+	}
+
+	log.Print("this the list of current chat groups ")
+	log.Print(grp)
+
+	return &chat.ChatGroupList{Groups: grp}, nil
+}
+
+// It returns a list of clients of a group.
+func (s *server) GetChatGroupClientList(ctx context.Context, in *chat.ChatGroup) (*chat.ChatClientList, error) {
+
+	grpname := in.Name
+
+	if !s.NotAvailableGroupName(grpname) {
+		return &chat.ChatClientList{}, errors.New("that group doesn't exist")
+	}
+
+	list := s.chatgroups[grpname].clients
+
+	log.Print("this is group " + grpname + " members list: ")
+	log.Print(list)
+
+	return &chat.ChatClientList{Clients: list}, nil
+}
+
+// Register will add the user to the server's collection of users (and by extension restrict the username).
+// It returns an empty object and an error.
+func (s *server) Register(ctx context.Context, in *chat.ChatClient) (*chat.Empty, error) {
+
+	name := in.Sender
+	if s.RegisteredClient(name) {
+		return nil, errors.New("that client already registered")
+	}
+
+	s.AddChatClient(name)
+	return &chat.Empty{}, nil
+}
+
+// removes a user from the server
+
+func (s *server) UnRegister(ctx context.Context, in *chat.ChatClient) (*chat.Empty, error) {
+
+	cl := in.Sender
+
+	log.Print("Unregistering client " + cl)
+
+	err := s.RemoveClient(cl)
+
+	if err != nil {
+		return nil, err
+	}
+	log.Print("Unregistered client " + cl)
+
+	return &chat.Empty{}, nil
+}
+
+// creates a chat group if the name is availabe.
+// It returns an empty object and an error.
+func (s *server) CreateChatGroup(ctx context.Context, in *chat.ChatGroup) (*chat.Empty, error) {
+
+	clName := in.Client
+	grpName := in.Name
+
+	log.Printf(clName + " is attempting creating " + grpName)
+
+	if !s.NotAvailableGroupName(grpName) {
+		s.AddChatGroup(grpName)
+		return &chat.Empty{}, nil
+	}
+
+	return &chat.Empty{}, errors.New("the group name is not available")
+}
+
+// let a user to an existing group.
+
+func (s *server) JoinChatGroup(ctx context.Context, in *chat.ChatGroup) (*chat.Empty, error) {
+
+	clName := in.Client
+	grpName := in.Name
+
+	log.Printf(clName + " is trying to joing group: " + grpName)
+
+	if s.NotAvailableGroupName(grpName) {
+		s.AddClientToChatGroup(clName, grpName)
+
+		return &chat.Empty{}, nil
+	}
+
+	return &chat.Empty{}, errors.New("a group with that name doesn't exist")
+}
+
+// LeaveRoom removes the user from their group.
+// It returns an empty object and an error.
+func (s *server) LeaveChatRoom(ctx context.Context, in *chat.ChatGroup) (*chat.Empty, error) {
+
+	clName := in.Client
+	grpName := in.Name
+
+	if !s.NotAvailableGroupName(grpName) {
+		return &chat.Empty{}, errors.New("group:" + grpName + " doesn't exist")
+
+	} else if !s.RegisteredClient(clName) {
+		return &chat.Empty{}, errors.New("the client name " + clName + " is not registered")
+	} else {
+		msg := chat.Message{Sender: clName, Receiver: grpName, Body: clName + " left chat!\n"}
+
+		s.BroadcastMessage(grpName, msg)
+		s.RemoveClientFromGroup(clName, grpName)
+		return &chat.Empty{}, nil
+	}
+}
+
+// Broadcast takes any messages that need to be sent and sorts them by group. It then
+// adds  messages to message channel of each member of a group.
+
+func (s *server) BroadcastMessage(grpName string, msg chat.Message) {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	for grp := range s.chatgroups {
+		log.Printf(grpName + ":")
+		if grp == grpName {
+			log.Printf(msg.Sender + " sent " + msg.Receiver + " a message: " + msg.Body)
+			for _, c := range s.chatgroups[grp].clients {
+
+				if c == msg.Sender && msg.Body == msg.Sender+" left chat!\n" {
+
+					s.chatclients[c].ch <- msg
+
+				} else if c != msg.Sender {
+
+					log.Printf(msg.Sender + "sending message to " + c + "...")
+					s.chatclients[c].ch <- msg
+				}
+			}
+		}
+	}
+}
+
+// ListenToClient listens on the incoming stream for any messages. It adds those messages to the channel.
+// It doesn't return anything.
+func Listen(stream chat.ChatService_RouteChatServer, messages chan<- chat.Message) {
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+		}
+		if err != nil {
+		} else {
+			log.Printf(msg.Sender + " sent " + msg.Receiver + " a message: " + msg.Body)
+			messages <- *msg
+		}
+
+	}
+
+}
+
+// RouteChat handles the routing of all messages on the stream.
+// It returns an error.
+func (s *server) RouteChat(stream chat.ChatService_RouteChatServer) error {
+
+	msg, err := stream.Recv()
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf(msg.Sender + " sent " + msg.Receiver + " a message: " + msg.Body)
+
+	outbox := make(chan chat.Message, 100)
+
+	go Listen(stream, outbox)
+
+	for {
+		select {
+		case outMsg := <-outbox:
+			s.BroadcastMessage(msg.Receiver, outMsg)
+		case inMsg := <-s.chatclients[msg.Sender].ch:
+			log.Println("Sending message to channel: ")
+			log.Println(s.chatclients[msg.Sender])
+			log.Println("Sending message to STREAM: ")
+			log.Println(stream)
+			stream.Send(&inMsg)
+		}
+	}
+}
+
+func (s *server) genToken() string {
+	tkn := make([]byte, 4)
+	rand.Read(tkn)
+	return fmt.Sprintf("%x", tkn)
+}
+func (s *server) Login(ctx context.Context, req *chat.ClientLoginRequest) (*chat.ClientLoginResponse, error) {
+	switch {
+	case req.Password != s.Password:
+		return nil, status.Error(codes.Unauthenticated, "password is incorrect")
+	case req.Name == "":
+		return nil, status.Error(codes.InvalidArgument, "username is required")
+	}
+
+	tkn := s.genToken()
+	s.setName(tkn, req.Name)
+
+	log.Printf(tkn + "," + req.Name + "has logged in")
+
+	return &chat.ClientLoginResponse{Token: tkn}, nil
+}
+
+//logout from server
+func (s *server) Logout(ctx context.Context, req *chat.ClientLogoutRequest) (*chat.ClientLogoutResponse, error) {
+	name, ok := s.deleteToken(req.Token)
+	if !ok {
+		return nil, status.Error(codes.NotFound, "token not found")
+	}
+	log.Printf(name + " logged out")
+	return new(chat.ClientLogoutResponse), nil
+}
+
+func (s *server) getName(tkn string) (string, bool) {
+	s.lock.RLock()
+	name, ok := s.clienttoken[tkn]
+	s.lock.RUnlock()
+	return name, ok
+}
+
+func (s *server) setName(tkn string, name string) {
+	s.lock.Lock()
+	s.clienttoken[tkn] = name
+	s.lock.Unlock()
+}
+
+func (s *server) deleteToken(tkn string) (name string, ok bool) {
+	name, ok = s.getName(tkn)
+
+	if ok {
+		s.lock.Lock()
+		delete(s.chatclients, tkn)
+		s.lock.Unlock()
+	}
+
+	return name, ok
+}
+
+func (s *server) extractToken(ctx context.Context) (tkn string, ok bool) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok || len(md[tokenHeader]) == 0 {
+		return "", false
+	}
+
+	return md[tokenHeader][0], true
+}
 
 func main() {
 
-	fmt.Println("hey grpchat!")
+	lis, err := net.Listen("tcp", port)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to listen %v", err)
 	}
+	log.Println("server listening on port" + port)
+	// Initializes the gRPC server.
+	s := grpc.NewServer()
+	var mlock = &sync.RWMutex{}
+	var clients = make(map[string]*Client)
+	var groups = make(map[string]*Group)
 
-	s := chat.Server{}
+	// Register the server with gRPC.
+	chat.RegisterChatServiceServer(s, &server{lock: *mlock, chatclients: clients,
+		chatgroups: groups, Host: port, Password: "nada"})
 
-	grpcServer := grpc.NewServer()
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 
-	chat.RegisterChatServiceServer(grpcServer, &s)
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
 	}
 }
